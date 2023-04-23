db05bcf0ddd7e4e50f95881866eb64a34bbd5dab
JasperC61 <m9703918@mail.ntust.edu.tw>
2023-04-23 23:15:35 +0800

    商品功能-實作商品查詢列表(設定分頁條件)

--- src/main/java/com/jasper/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/jasper/springbootmall/dao/impl/ProductDaoImpl.java
@@ -27,6 +27,7 @@ public class ProductDaoImpl  implements ProductDao {
     public List<Product> getProducts(ProductQueryParams productQueryParams) {
         String sql="select product_id ,product_name,category,image_url, price,stock, description,created_date, last_modified_date from product where 1=1";
         Map<String,Object> map=new HashMap<>();
+        //查詢條件
         if(productQueryParams.getCategory() !=null){
             sql=sql+" and category=:category";
             map.put("category",productQueryParams.getCategory().name());
@@ -35,8 +36,13 @@ public class ProductDaoImpl  implements ProductDao {
             sql=sql+" and product_name   like :search";
             map.put("search","%"+ productQueryParams.getSearch()+ "%");
         }
-
+        //排序
         sql=sql+" ORDER BY "+productQueryParams.getOrderBy()+" "+productQueryParams.getSort();
+        //分頁
+        sql=sql+" LIMIT :limit OFFSET :offset";
+        map.put("limit", productQueryParams.getLimit());
+        map.put("offset", productQueryParams.getOffset());
+
 
         List<Product> productList=namedParameterJdbcTemplate.query(sql,map,new ProductRowMapper());
 
