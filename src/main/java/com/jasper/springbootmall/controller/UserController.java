04a35a07d2fa9658ce9005a614b3221183a786ea
JasperC61 <m9703918@mail.ntust.edu.tw>
2023-04-26 10:45:55 +0800

    商品功能-帳號功能(登入判斷)

--- src/main/java/com/jasper/springbootmall/controller/UserController.java
+++ src/main/java/com/jasper/springbootmall/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.jasper.springbootmall.controller;
 
 
+import com.jasper.springbootmall.dto.UserLoginRequest;
 import com.jasper.springbootmall.dto.UserRegisterRequest;
 import com.jasper.springbootmall.model.User;
 import com.jasper.springbootmall.service.UserService;
@@ -16,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserService userService;
-
+    //創建帳號
     @PostMapping("/users/register")
     public ResponseEntity<User> register(@RequestBody @Valid UserRegisterRequest userRegisterRequest){
         //接收前端傳入參數放入userService中register方法,在資料庫中創建userRegisterRequest生成userId
@@ -25,6 +26,14 @@ public class UserController {
           User user=userService.getUserById(userId);
           //將商品數據取回傳給前端
           return  ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+    @PostMapping("/user/login")
+   //登入功能
+    public ResponseEntity<User> login(@RequestBody @Valid UserLoginRequest userLoginRequest){
+        //實作login方法,使用人所使用密碼可否成功登入,用user接住返回值
+        User user=   userService.login(userLoginRequest);
+        //返回值狀態碼給前端
+        return ResponseEntity.status((HttpStatus.OK)).body(user);
     }
 
 }
--- src/main/java/com/jasper/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/jasper/springbootmall/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.jasper.springbootmall.service.impl;
 
 import com.jasper.springbootmall.dao.UserDao;
+import com.jasper.springbootmall.dto.UserLoginRequest;
 import com.jasper.springbootmall.dto.UserRegisterRequest;
 import com.jasper.springbootmall.model.User;
 import com.jasper.springbootmall.service.UserService;
@@ -35,5 +36,23 @@ public class UserServiceImpl implements UserService {
 
        //建立帳號
         return userDao.createUser(userRegisterRequest);
+    }
+    //檢查前端輸入密碼與資料庫一致?
+    @Override
+    public User login(UserLoginRequest userLoginRequest) {
+        User user=userDao.getUserByEmail(userLoginRequest.getEmail());
+            if(user==null){
+                log.warn("該email {} 尚未註冊",userLoginRequest.getEmail());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
+            if(user.getPassword().equals(userLoginRequest.getPassword())){
+                return  user;
+            }else {
+                log.warn("該email {} 密碼不正確",userLoginRequest.getEmail());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+            }
+
     }
 }
