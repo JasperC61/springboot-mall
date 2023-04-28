967abe856c1560fe25332b614a001695d52f610d
JasperC61 <m9703918@mail.ntust.edu.tw>
2023-04-28 10:22:48 +0800

    商品功能-檢查user是否存在,扣除商品庫存

--- src/main/java/com/jasper/springbootmall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/jasper/springbootmall/service/impl/OrderServiceImpl.java
@@ -2,26 +2,37 @@ package com.jasper.springbootmall.service.impl;
 
 import com.jasper.springbootmall.dao.OrderDao;
 import com.jasper.springbootmall.dao.ProductDao;
+import com.jasper.springbootmall.dao.UserDao;
 import com.jasper.springbootmall.dao.impl.ProductDaoImpl;
 import com.jasper.springbootmall.dto.BuyItem;
 import com.jasper.springbootmall.dto.CreateOrderRequest;
 import com.jasper.springbootmall.model.Order;
 import com.jasper.springbootmall.model.OrderItem;
 import com.jasper.springbootmall.model.Product;
+import com.jasper.springbootmall.model.User;
 import com.jasper.springbootmall.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 @Component
 public class OrderServiceImpl implements OrderService {
+    private  final  static Logger log= LoggerFactory.getLogger(OrderServiceImpl.class);
     @Autowired
     private OrderDao orderDao;
     @Autowired
     private ProductDaoImpl productDao;
+    @Autowired
+    private UserDao userDao;
 
     //分別在order及order_item表中取的數據,所以會call orderDao兩次
     @Override
@@ -36,7 +47,13 @@ public class OrderServiceImpl implements OrderService {
     @Transactional //復原資料庫操作,兩張資料庫數據同時發生或同時不發生 類似ROLL BACK,避免數據不一致
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+         //檢查user是否存在
+        User user=userDao.getUserById(userId);
 
+        if(user== null){
+            log.warn("該userId{} 不存在",userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
 
         int totalAmount=0;
         //創建list
@@ -46,7 +63,21 @@ public class OrderServiceImpl implements OrderService {
           //根據前端傳入getProductId()去Product資料庫查詢數據,順便注入productDao bean
          Product product= productDao.getProductById(buyItem.getProductId());
 
-         //計算產品總價錢
+          //檢查product是否存在,庫存是否足夠
+             if(product==null){
+                 log.warn("商品{} 不存在" ,buyItem.getProductId());
+                 throw  new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+             } else if (product.getStock()< buyItem.getQuantity()) {
+                 log.warn("商品{} 庫存數量不足,無法購買,庫存剩餘{},欲購買數量{}",
+                         buyItem.getProductId(),product.getStock(),buyItem.getQuantity());
+                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+             }
+             //扣除商品庫存
+             productDao.updateStock(product.getProductId(),product.getStock()-buyItem.getQuantity());
+
+
+             //計算產品總價錢
              //計算user購買數量及價格,並計算品項價錢
              int amount=buyItem.getQuantity()*product.getPrice();
              //加總每項產品購買數量總價
